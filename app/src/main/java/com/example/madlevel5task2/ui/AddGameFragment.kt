f1a754916715346b77e4ed189a0d3fcb74942526
dylan2050 <dyhayg@gmail.com>
2020-10-24 13:00:48 +0200

    created add_game fragment and gamebacklog fragment

--- app/src/main/java/com/example/madlevel5task2/ui/AddGameFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/AddGameFragment.kt
@@ -1,10 +1,11 @@
-package com.example.madlevel5task2
+package com.example.madlevel5task2.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.madlevel5task2.R
 
 class AddGameFragment : Fragment() {
 
